0ad5b975a811de4748b45f005792878fdd8e3a49
Rendi Musahikin Abdulah <rendimusahikinabdulah.27@gmail.com>
2026-02-04 18:23:31 +0700

    update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Tambahkan ini agar jadi file statis
   images: {
+    unoptimized: true, // GitHub Pages tidak bisa memproses optimasi gambar otomatis Next.js
     remotePatterns: [
       {
         protocol: 'https',
